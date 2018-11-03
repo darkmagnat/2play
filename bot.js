47e60874a16ac8580c764f26e6cadc88f2cab327
darkmagnat <43116040+darkmagnat@users.noreply.github.com>
2018-11-03 13:11:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -358,7 +358,7 @@ client.user.setUsername(argresult).then
     message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
 return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
 } else
-  if (message.content.startsWith(adminprefix + 'avatar')) {
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
 client.user.setAvatar(argresult);
   message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
       } else     
